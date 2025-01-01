79695e8e6c518b7e5b9fee7650066a2de03227b4
Yash <yashkhamkar.sae.it@gmail.com>
2025-01-01 14:40:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const db=require('./utils/db');
-const leadsRoutes=require('./routes/leads.routes');
-const contactsRoutes=require('./routes/contacts.routes');
-const interactionsRoutes=require('./routes/interactions.routes');
-const kamRoutes=require('./routes/kam.routes');
-const performanceRoutes=require('./routes/performance.routes');
+const db = require("./utils/db");
+const leadsRoutes = require("./routes/leads.routes");
+const contactsRoutes = require("./routes/contacts.routes");
+const interactionsRoutes = require("./routes/interactions.routes");
+const kamRoutes = require("./routes/kam.routes");
+const performanceRoutes = require("./routes/performance.routes");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
@@ -20,8 +25,8 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.use('/api/leads',leadsRoutes);
-app.use('/api/contacts',contactsRoutes);
-app.use('/api/interactions',interactionsRoutes);
-app.use('/api/auth',kamRoutes);
-app.use('/api/performance',performanceRoutes);
+app.use("/api/leads", leadsRoutes);
+app.use("/api/contacts", contactsRoutes);
+app.use("/api/interactions", interactionsRoutes);
+app.use("/api/auth", kamRoutes);
+app.use("/api/performance", performanceRoutes);
